f819d374903c0f031cd61287d2178a1004925518
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2020-01-16 19:30:48 +0100

    Initial commit

---
+++ programko-LAPTOP-MDC0TG59/říjen/xdd.java
@@ -0,0 +1,41 @@
+public class xdd{
+
+    public static void main(String []args){
+       int hrnec;  //Deklarace
+       hrnec = 1; //Inicializace
+       int numberRun = 0;
+
+           System.out.println("Můj první program - špagety");
+           System.out.println("Vezmi hrnec z poličky");
+           System.out.println("Velikost hrnce je: " + hrnec );
+           while (hrnec <=2 && numberRun !=2 ){
+               if (hrnec >= 2) {
+
+           System.out.println("Natoč 1 l vody");
+           System.out.println("Odnes hrnec na sporák");
+           System.out.println("Zapni sporák");
+           System.out.println("Nasyp 3g soli");
+           System.out.println("Počkej než se začne vařit voda");
+           System.out.println("Vem špagety z poličky");
+           System.out.println("Rozbal balíček špaget");
+           System.out.println("Nasyp špagety do hrnce");
+           System.out.println("Přidej lžičku slunečnicového oleje");
+           System.out.println("Počkej 10 min");
+           System.out.println("Vypni sporák");
+           System.out.println("");
+           System.out.println("");
+           System.out.println("");
+           System.out.println("");
+           System.out.println("");
+           System.out.println("");
+           System.out.println("");
+               }
+               else{
+                   System.out.println("Nemožné pokračovat");
+                       hrnec = hrnec + 1;
+                   System.out.println("hrnec je: "+ hrnec);
+               }
+             numberRun++;
+           }
+    }
+}
