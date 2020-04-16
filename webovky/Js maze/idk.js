a9b076df60d3b904e2680fd292efb5a853dcad93
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2020-04-16 12:26:14 +0200

    funguje to, ale nefunguje to

--- webovky/Js maze/idk.js
+++ webovky/Js maze/idk.js
@@ -7,7 +7,26 @@ let map = [[1,1,1,1,1,1,1,1],
            [1,3,0,1,1,1,0,1],
            [1,1,1,1,1,1,1,1]];
 
-let table = "<table>";
+let map1 = [[1,1,1,1,1,1,1,1],
+            [1,1,1,0,1,1,0,1],
+            [1,4,0,0,0,0,0,1],
+            [1,1,1,0,1,0,1,1],
+            [1,0,0,0,0,0,0,1],
+            [1,0,1,1,1,1,0,1],
+            [1,0,3,1,1,1,1,1],
+            [1,1,1,1,1,1,1,1]];
+
+let map2 = [[1,1,1,1,1,1,1,1],
+            [1,4,0,1,1,0,1,1],
+            [1,1,0,1,0,0,0,1],
+            [1,1,0,1,0,1,0,1],
+            [1,1,0,0,0,1,0,1],
+            [1,0,0,1,0,1,0,1],
+            [1,1,1,0,0,1,3,1],
+            [1,1,1,1,1,1,1,1]];
+                     
+let div = "<div id=huhu>";
+let table = "<table id=gaga>";
 
 for(let i = 0; i < map.length; i++){
     let row = '<tr>';
@@ -29,6 +48,53 @@ for(let i = 0; i < map.length; i++){
     row += "</tr>";
     table += row;
 }
+table += "</table></div>";
+div += table
+document.write(div);
 
-table += "</table>";
-document.write(table);
+
+
+function down(){
+    let delet = document.getElementById("huhu");
+    delet.remove();
+    let count = 0
+    for(let i = 0; i < map.length; i++){
+        for(let x = 0; x < map.length; x++){
+            let wall = i + 1;
+            if(map[i][x] == 4 && count != 1 && map[wall][x] != 1){
+                map[i][x] = 0;
+                let bruh = i + 1;
+                map[bruh][x] = 4;
+                count++;
+            }
+        }
+    }
+    
+    div = "<div id=huhu>";
+    table = "<table id=gaga>";
+    
+    for(let i = 0; i < map.length; i++){
+        row = "<tr>";
+        for(let x = 0; x < map[i].length; x++){
+            if(map[i][x] == 0){
+                row += '<td><img src="JsSprity/Filler.png"' + map[i][x] + '</td>';  
+            }
+            
+            else if(map[i][x] == 1){
+                row += '<td><img src="JsSprity/Kámen.png"' + map[i][x] + '</td>';
+            }
+            else if(map[i][x] == 3){
+                row += '<td><img src="JsSprity/Kytka.png"' + map[i][x] + '</td>';  
+            }
+            else if(map[i][x] == 4){
+                row += '<td><img src="JsSprity/Dinosajrus.png"' + map[i][x] + '</td>';  
+            }
+        }
+        row += "</tr>";
+        table += row;
+    }
+    table += "</table></div>";
+    div += table;
+    document.write(div);
+
+};
